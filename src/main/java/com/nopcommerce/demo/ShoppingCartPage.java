5862f9e9c7c7817fe39fb4d99be6bfd95cc6e264
kpnarola <kpnarola@gmail.com>
2019-01-15 21:47:08 +0000

    shopping cart page

--- src/main/java/com/nopcommerce/demo/ShoppingCartPage.java
+++ src/main/java/com/nopcommerce/demo/ShoppingCartPage.java
@@ -3,14 +3,17 @@ package com.nopcommerce.demo;
 import org.openqa.selenium.By;
 
 public class ShoppingCartPage extends Utils {
+    LoadProperty loadProperty = new LoadProperty();
+    By checkOutButton = By.id("checkout");
+    By termsOfServiceButton = By.id("termsofservice");
 
 //Add
     public void clickCheckOutButton()
 
     {
-        clickOnElement(By.id("checkout"));
+        clickOnElement(checkOutButton);
     }
 
-    public void clickOnTermsOfService(){clickOnElement(By.id("termsofservice"));}
+    public void clickOnTermsOfService(){clickOnElement(termsOfServiceButton);}
 }
 
