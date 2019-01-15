7f7e33a1972ad0326d8bfd491d3d385b28559872
kpnarola <kpnarola@gmail.com>
2019-01-15 22:39:57 +0000

    shopping cart page

--- src/main/java/com/nopcommerce/demo/Utils.java
+++ src/main/java/com/nopcommerce/demo/Utils.java
@@ -9,6 +9,7 @@ import org.testng.Assert;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.NoSuchElementException;
 
 public class Utils extends BasePage {
 
@@ -17,7 +18,7 @@ public class Utils extends BasePage {
         driver.findElement(by).click();
     }
 
-    //Reusable method for SendKey
+    //Reusable method for Enter text with SendKey
     public static void enterText(By by, String text) {
         driver.findElement(by).sendKeys(text);
     }
@@ -46,42 +47,94 @@ public class Utils extends BasePage {
 
     public static void getText(By by) {
         driver.findElement(by).getText();
+        return;
     }
-
+    //Reusable Method For assert
     public static void assertByGetText(String expectedResult,By by, String errorMessage) {
 
         Assert.assertEquals(expectedResult, driver.findElement(by).getText(), errorMessage);
     }
+
     //send/type any data after clearing field/box
     public static void typeTextWithClearing(By by,String text){
         driver.findElement(by).clear();
         driver.findElement(by).sendKeys(text);
     }
+    //send/type any data without clearing field/box
     public static void typeTextWithoutClearing(By by,String text){
         driver.findElement(by).sendKeys();
     }
-    // Re usable method for drop down selection
+
+    // drop down selection  by visible text
     public static void selectText(By by,String text){
 
         new Select(driver.findElement(by)).selectByVisibleText(text);
     }
 
+    // Drop down selection by value
     public static void selectValue(By by,String value){
 
         Select select = new Select(driver.findElement(by));
         select.selectByValue(value);
     }
 
+    // Drop down selection by index number
     public static void selectIndex(By by,int indexNumber){
         new Select(driver.findElement(by)).selectByIndex(indexNumber);
 
     }
 
     //get text by attribute
-
     public static String getTextByAttribute(By by,String attributeName){
         String text = driver.findElement(by).getAttribute(attributeName);
         return text;
+    }
+
+    //Checking WebElement present in DOM
+    public  static boolean isElenetPresent (By by){
+        try {
+            driver.findElement(by);
+            return true;
+
+        }catch(NoSuchElementException e){
+            return false;
+        }
+    }
+
+    //Checking WebElement displayed or not
+    public static boolean isElementPresent(By by)
+    {
+        try {
+            return driver.findElement(by).isDisplayed();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+//Wait for fixed time given in seconds
+    public static void sleep(int second) {
+        try {
+            Thread.sleep(second, 1000);
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+        //Try to click element three times if not available in first go
+
+    public static boolean retrayingfindclick(By by){
+        boolean result = false;
+        int attemps = 0;
+        while (attemps<3){
+            try {
+                Thread.sleep(1000);
+                driver.findElement(by).click();
+                result = true;
+                break;
+            }catch (Exception e){}attemps ++;
+        }return  result;
+    }
+
+    
     }
 
 
@@ -95,5 +148,4 @@ public class Utils extends BasePage {
 
 
 
-}
 
--- src/test/java/com/nopcommerce/demo/BaseTest.java
+++ src/test/java/com/nopcommerce/demo/BaseTest.java
@@ -55,7 +55,7 @@ public class BaseTest extends Utils {
         }
 
 
-//        driver.quit();
+       driver.quit();
     }
 
 
